859c2cb5fa5666fb6a1154f80dffdb820c6a751b
Jephtah Paul <jeffgraphics@gmail.com>
2023-06-06 11:10:29 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write Your Code Here
+            //string myString = "Hello World";
+            //decimal myDecimal = 0.77m;
+            //bool myBoolean = true;
+            
+            //Write You Code Above This Line
+            //Console.WriteLine(myString.GetType());
+            //Console.WriteLine(myDecimal.GetType());
+            //Console.WriteLine(myBoolean.GetType());
+
+            string[] myGroceryArray = new string[2];
+
+            myGroceryArray[0] = "Guacamole";
+            myGroceryArray[1] = "Guacamole";
+            
+            Console.WriteLine(myGroceryArray[0]);
+            Console.WriteLine(myGroceryArray[1]);
+            //Console.WriteLine(myGroceryArray[2]);
+        }
+    }
+}
